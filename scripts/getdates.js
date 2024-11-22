fded33838e0d5732a51906c2f35e04785d3f754b
gerson <gersonmoliver@gmail.com>
2024-11-21 20:46:30 -0800

    W01 Assignment: Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+// Get the last modified date
+const currYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+
+document.getElementById("lastModified").textContent += lastModified;
+document.getElementById("currentYear").textContent = currYear;
+
+
+
+
