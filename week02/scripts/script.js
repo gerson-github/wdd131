a7097d8162e166562ca465fa7a8ce9f16f46ef10
gerson <gersonmoliver@gmail.com>
2024-11-23 05:08:19 -0800

    implemented myItem input

--- week02/scripts/script.js
+++ week02/scripts/script.js
@@ -3,33 +3,42 @@ const input = document.querySelector('input');
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
-  const myItem = input.value;
-  input.value = '';
-
-  const listItem = document.createElement('li');
-  const listText = document.createElement('span');
-  const listBtn = document.createElement('button');
-
-  listItem.appendChild(listText);
-  listText.textContent = myItem;
-  listItem.appendChild(listBtn);
-  listBtn.textContent = 'Delete';
-  list.appendChild(listItem);
-
-  listBtn.addEventListener('click', () => {
-    list.removeChild(listItem);
-  });
-
-  input.focus();
-});
-
-const listItem = document.createElement('li');
-const listText = document.createElement('span');
-const listBtn = document.createElement('button');
+  const myItem = input.value.trim();
  
-listItem.appendChild(listText);
-listText.textContent = "gerson";
-list.appendChild(listItem)
+
+  if (myItem !== '')
+  {
+
+    input.value = '';
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+  
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = 'Delete';
+    list.appendChild(listItem);
+  
+    listBtn.addEventListener('click', () => {
+      list.removeChild(listItem);
+    });
+      
+  };
+
+});
+  
+  input.focus();
+
+  
+
+// const listItem = document.createElement('li');
+// const listText = document.createElement('span');
+// const listBtn = document.createElement('button');
+ 
+// listItem.appendChild(listText);
+// listText.textContent = "gerson";
+// list.appendChild(listItem)
 
 // const body = document.body;
 // const ppp = document.createElement("p");
