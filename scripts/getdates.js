038149af422cf548982bc0fde1a94658aeadadcf
gerson <gersonmoliver@gmail.com>
2024-12-07 05:00:44 -0800

    minificado

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,1 @@
-// Get the last modified date
-const currYear = new Date().getFullYear();
-const lastModified = document.lastModified;
-
-
-document.getElementById("lastModified").textContent += lastModified;
-document.getElementById("currentYear").textContent = currYear;
-
-
-
-
+const currYear=(new Date).getFullYear(),lastModified=document.lastModified;document.getElementById("lastModified").textContent+=lastModified,document.getElementById("currentYear").textContent=currYear;
