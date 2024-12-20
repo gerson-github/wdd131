0f48b54a520cbd2c44fe4c7aa357d6ba8813a780
gerson <gersonmoliver@gmail.com>
2024-12-19 18:27:17 -0800

    new deployments

--- eRestaurant/scripts/eRestaurant.js
+++ eRestaurant/scripts/eRestaurant.js
@@ -1,6 +1,12 @@
 // Get the current year
 const currYear = (new Date()).getFullYear();
 
+const currentDate = new Date();
+const formattedDate = currentDate.toLocaleString(); 
+
+document.getElementById("current-date").textContent += formattedDate;
+
+
 // Get the last modified date of the document
 const lastModified = document.lastModified;
 
