176e7d319a60b100571b94e84a33c2cc061ca4e3
gerson <gersonmoliver@gmail.com>
2024-12-19 19:43:17 -0800

    implementado mobile

--- eRestaurant/scripts/eRestaurant.js
+++ eRestaurant/scripts/eRestaurant.js
@@ -15,3 +15,25 @@ document.getElementById("lastModified").textContent += lastModified;
 
 // Update the "currentYear" element's text content
 document.getElementById("currentYear").textContent = currYear;
+
+
+// JavaScript to toggle hamburger menu visibility
+const hamburgerMenu = document.getElementById('hamburgerMenu');
+const hamburgerOptions = document.getElementById('hamburgerOptions');
+const menuContainer = document.getElementById('menu-container');
+const navMenu = document.getElementById('nav-menu');
+
+
+// Toggle hamburger menu on click
+hamburgerMenu.addEventListener('click', () => {
+    if (hamburgerOptions.style.display === 'block') {
+        hamburgerOptions.style.display = 'none';  // Hide menu
+        menuContainer.style.display = 'block';
+        navMenu.style.display = 'block';
+    } else {
+        hamburgerOptions.style.display = 'block';  // Show menu
+        menuContainer.style.display = 'none';
+        navMenu.style.display = 'none';
+    }
+});
+
